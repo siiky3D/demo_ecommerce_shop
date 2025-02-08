620101b9ce3a4b1db96aa5443dddb6f8a8e9867f
Siiky3 <kittichonc002@gmail.com>
2025-02-08 14:25:34 +0700

    change error import test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:demo_ecommerce_shop/src/app.dart';
+import 'package:demo_app/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
